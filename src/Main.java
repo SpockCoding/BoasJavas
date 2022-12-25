a0a8e61cc85870b851ec3b771b64968651484cd0
spockiscoding <valdir.cruz@gmail.com>
2022-12-25 01:21:46 -0300

    Primeiro Commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+			int day = 0;
+			while (day >= 24 && day < 26);
+			{
+				System.out.println ("Boas Festas!!! Que a graça esteja convosco");
+				System.out.println ("Jesus abençoe a todos vós");
+		
+			}
+	}
+}
